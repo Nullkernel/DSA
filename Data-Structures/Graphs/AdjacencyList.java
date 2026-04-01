9c76d91a68ab32f5f29e19920a4a6213d2c42397
Amal Bijoy <amalbijoy2007@gmail.com>
2026-04-01 15:00:08 +0530

    Fix formatting and improve output message in AdjacencyList

--- Data-Structures/Graphs/AdjacencyList.java
+++ Data-Structures/Graphs/AdjacencyList.java
@@ -8,24 +8,24 @@ public class AdjacencyList {
         adj.get(j).add(i);
     }
     public static void displayAdjList(List<List<Integer>> adj) {
-        for (int i = 0; i < adj.size(); i ++) {
+        for (int i = 0; i < adj.size(); i++) {
             System.out.print(i +" --> ");
             for (int j : adj.get(i)) {
-                System.out.print(j +" ");
+                System.out.print(j + " ");
             }
         }
     }
     public static void main(String[] args) {
         int v = 4;
         List<List<Integer>> adj = new ArrayList<>(v);
-        for (int i = 0; i < v;i++) {
+        for (int i = 0; i < v; i++) {
             adj.add(new ArrayList<>());
         }
         addEdge(adj, 0, 1);
         addEdge(adj, 0, 2);
         addEdge(adj, 1, 2);
         addEdge(adj, 2, 3);
-        System.out.println("Adjaceny List representation : ");
+        System.out.println("Adjaceny List Representation: ");
         displayAdjList(adj);
     }
 }
