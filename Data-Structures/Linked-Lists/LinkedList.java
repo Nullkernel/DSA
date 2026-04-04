004907828191beedfb9f093a13aef861232a5bfc
Amal Bijoy <amalbijoy2007@gmail.com>
2026-04-04 11:43:12 +0530

    Update and rename LinkedLists.java to LinkedList.java

--- Data-Structures/Linked-Lists/LinkedList.java
+++ Data-Structures/Linked-Lists/LinkedList.java
@@ -8,7 +8,7 @@ class Node {
         this.next = null;
     }
 }
-class Singlylinkedlist {
+class LinkedList {
     private Node head;
     //----Insert At Beginning----
     public void insertAtBeginning(int data) {
